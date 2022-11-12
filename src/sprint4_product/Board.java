935cbc3fff0c7b89d9e5969a45afb88595d6a1e5
Ashna <aa57c@umsystem.edu>
2022-11-11 21:13:25 -0600

    started sprint 4. added buttons for human and computer

---
+++ src/sprint4_product/Board.java
@@ -0,0 +1,253 @@
+package sprint4_product;
+
+public class Board {
+	public enum GameState {PLAYING, DRAW, RED_WON, BLUE_WON}
+	private String gameMode = "";
+	private GameState currentGameState;
+	private Cell[][] grid;
+	private char player_symbol = ' ';
+	private String player_color = "red";
+	private int redWins = 0;
+	private int blueWins = 0;
+	private int boardsize;
+	
+	
+	public void resetWins() {
+		this.redWins = 0;
+		this.blueWins = 0;
+	}
+	public void setGameMode(String mode) {
+		this.gameMode = mode;
+	}
+	public String getGameMode() {
+		return this.gameMode;
+	}
+	
+	public void setPlayerSymbol(char symbol) {
+		this.player_symbol = symbol;
+	}
+	
+	
+	public void setPlayerColor(String color) {
+		this.player_color = color;
+	}
+	
+	public String getPlayerColor() {
+		return this.player_color;
+	}
+	
+	public char getPlayerSymbol() {
+		return this.player_symbol;
+	}
+
+	
+	
+	public void setBoardsize(int size) {
+		this.boardsize = size;
+		this.grid = new Cell[size][size];
+		InitializeGrid();
+	}
+	
+	public int getBoardsize() {
+		return this.boardsize;
+	}
+	
+	
+	public Board() {
+		grid = new Cell[this.boardsize][this.boardsize];
+		InitializeGrid();
+	}
+	
+	public void InitializeGrid() {
+		for(int row = 0; row < this.boardsize; row++ ) {
+			for(int col = 0; col < this.boardsize; col++) {
+				this.grid[row][col] = new Cell();
+			}
+		}
+		this.currentGameState = GameState.PLAYING;
+		
+	}
+	
+	
+	public int getCellSym(int row, int column) {
+		if (row >= 0 && row < this.boardsize && column >= 0 && column < this.boardsize) {
+			return this.grid[row][column].getSym();
+		}
+		else {
+			return -1;
+		}
+
+	}
+	public String getCellClr(int row, int column) {
+		if (row >= 0 && row < this.boardsize && column >= 0 && column < this.boardsize)
+			return this.grid[row][column].getClr();
+		else
+			return "None";
+		
+	}
+	public Cell getCellContents(int row, int column) {
+		if (row >= 0 && row < this.boardsize && column >= 0 && column < this.boardsize) {
+			return this.grid[row][column];
+		}
+		else {
+			return null;
+		}
+		
+	}
+	public void makeMove(int row, int column) {
+		if (row >= 0 && row < this.boardsize && column >= 0 && column < this.boardsize
+				&& this.grid[row][column].getSym() == 0) {
+			this.grid[row][column].setMyPair(this.player_symbol == 'S' ? 1 : 2, this.player_color);
+			updateGameState(this.grid[row][column], row, column);
+			this.player_color = (this.player_color == "red")? "blue" : "red";
+			//this is just for testing purposes (I don't know if this actually matters while playing the game,
+			//since it still alternates regardless
+			this.player_symbol = (this.player_symbol == 'S') ? 'O' : 'S';
+		}
+	}
+
+	private void updateGameState(Cell turn, int row, int column) {
+		//if there is a win from either player AND the game mode is simple
+		if(hasWon(turn, row, column) && this.gameMode == "Simple Game") {
+			//verifies whether red or blue won
+			this.currentGameState = (turn.getClr() == "red") ? GameState.RED_WON : GameState.BLUE_WON;
+			//resets the player symbol and player color
+			setPlayerSymbol(' ');
+			setPlayerColor("red");
+		}
+		//if there is win from either player AND the game mode is general
+		else if(hasWon(turn, row, column) && this.gameMode == "General Game") {
+			//count red wins depending on the turn just played
+			this.redWins += (turn.getClr() == "red") ? 1 : 0;
+			//count blue wins depending on the turn just played
+			this.blueWins += (turn.getClr() == "blue") ? 1 : 0;
+		}
+		//if the board is full AND the game mode is simple, the game is a draw
+		else if(isBoardFull() && this.gameMode == "Simple Game") {
+			this.currentGameState = GameState.DRAW;
+			setPlayerSymbol(' ');
+			setPlayerColor("red");
+		}
+		//if the board is full AND the game mode is general
+		else if(isBoardFull() && this.gameMode == "General Game") {
+			//if red has more wins than blue player, red has won
+			if(this.redWins > this.blueWins) {
+				this.currentGameState = GameState.RED_WON;
+				setPlayerSymbol(' ');
+				setPlayerColor("red");
+				resetWins();
+			}
+			//if blue has more wins than the red player, blue has won
+			else if(this.blueWins > this.redWins) {
+				this.currentGameState = GameState.BLUE_WON;
+				setPlayerSymbol(' ');
+				setPlayerColor("red");
+				resetWins();
+			}
+			//if blue and red have the same amount of wins, the game is a draw
+			else if(this.redWins == this.blueWins) {
+				this.currentGameState = GameState.DRAW;
+				setPlayerSymbol(' ');
+				setPlayerColor("red");
+				resetWins();
+			}
+		}
+	}
+	private boolean isBoardFull() {
+		for (int row = 0; row < this.boardsize; row++) {
+			for (int col = 0; col < this.boardsize; col++) {
+				if (this.grid[row][col].getSym() == 0) {
+					return false; // an empty cell found, not draw
+				}
+			}
+		}
+		return true;
+	}
+	private boolean isValidCell(int r, int c) {
+		//checks to see if the neighbors of the turn just played are within the range of the board
+		return (r >= 0 && r < this.boardsize && c >= 0 && c < this.boardsize);
+		
+	}
+	private boolean isMatch(int r, int c, int sym, String clr) {
+		//checks to see if second or third cell in pattern matches the symbol and color of the turn just played
+		return(this.grid[r][c].getSym() > 0 && this.grid[r][c].getSym() == sym && this.grid[r][c].getClr() == clr);
+	}
+	private int getThirdCell(int start, int stop, int sym) {
+		int thirdCell = 0;
+		int Change;
+		//if symbol is S, then find third cell in the same direction because S is last symbol in pattern
+		if(sym == 1) {
+			//finding the direction of pattern
+			Change = start - stop;
+			//apply change to second cell to find third cell
+			thirdCell = start + Change;
+		}
+		//if symbol is O, then find third cell in the reverse direction because O is middle symbol in pattern
+		else if(sym == 2) {
+			//finding the direction of pattern (negative is reverse)
+			Change = (start - stop) * -2;
+			//apply change to second cell to find third cell
+			thirdCell = start + Change;
+		}
+		return thirdCell;
+	}
+	//function stub for determining wins
+	private boolean hasWon(Cell turn, int row, int col) {
+		//set second symbol and color based on turn (the target is the first space found after turn is made)
+		int SecondSym = turn.getSym() == 1 ? 2 : 1;
+		String SecondClr = turn.getClr() == "red" ? "red" : "blue";
+		int turnSym = this.grid[row][col].getSym();
+		
+		
+		//testing printing statements
+		/*
+		 * //String turnClr = this.grid[row][col].getClr();
+		System.out.println("Player's Turn: (" + row + "," + col + ")");
+		if(turnSym == 1) {
+			System.out.println("Player's Color: " + turnClr + ", Player's Symbol: S");
+		}
+		else {
+			System.out.println("Player's Color: " + turnClr + ", Player's Symbol: O");
+		}
+		System.out.println("Neighbors of Player...");
+		*/
+		//loop through all neighbor cells
+		for(int r = row - 1; r <= row + 1; r++) {
+			for(int c = col - 1; c <= col + 1; c++) {
+				//if the neighbor is the same as turn, then skip
+				if(r == row && c == col) {
+					continue;
+				}
+				//if the neighbor is within range of board
+				else if(isValidCell(r, c)) {
+					//System.out.print("(" + r + "," + c + ")");
+					//if the space the neighbor is in is occupied and it matches the target, found the second symbol after turn is placed
+					if(isMatch(r, c, SecondSym, SecondClr)) {
+						//System.out.println("Found Second Symbol");
+						//if the current symbol (current turn) is S, then find change of target coord and current coord
+						//1 represents symbol S
+						//2 represent symbol O
+						
+						int thirdCellRow = getThirdCell(r, row, turnSym);
+						int thirdCellCol = getThirdCell(c, col, turnSym);
+						//if the third cell is within range of board
+						if(isValidCell(thirdCellRow, thirdCellCol)) {
+							if(isMatch(thirdCellRow, thirdCellCol, 1, SecondClr)) {
+								//System.out.println("(" + thirdCellRow + "," + thirdCellCol + "), found winner!);
+								return true;
+									
+							}
+						}
+							
+					}
+				}
+			}
+		}
+		return false;
+
+	}
+	public GameState getGameState() {
+		return currentGameState;
+	}
+
+}
---
+++ src/sprint4_product/SOS_GUI.java
@@ -0,0 +1,457 @@
+package sprint4_product;
+
+import java.awt.BasicStroke;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import sprint4_product.Board.GameState;
+
+
+@SuppressWarnings("serial")
+
+
+public class SOS_GUI extends JFrame {
+	public static final int CELL_SIZE = 60; 
+	public static final int GRID_WIDTH = 8; 
+	public static final int GRID_WIDHT_HALF = GRID_WIDTH / 2; 
+
+	public static final int CELL_PADDING = CELL_SIZE / 6;
+	public static final int SYMBOL_SIZE = CELL_SIZE - CELL_PADDING * 2; 
+	public static final int SYMBOL_STROKE_WIDTH = 8;
+	
+	private int CANVAS_WIDTH;
+	private int CANVAS_HEIGHT;
+	
+	private Canvas canvas;
+	private Board board;
+
+	//Panel for top of game window (game options)
+	JPanel gameOptions = new JPanel(new FlowLayout());
+	JTextField tf = new JTextField(3);
+	JLabel sizeFieldTxt = new JLabel("Board Size");
+	JLabel title = new JLabel("SOS");
+	ButtonGroup gameType = new ButtonGroup();
+	JRadioButton simple = new JRadioButton();
+	JRadioButton general = new JRadioButton();
+
+	
+	//
+	//Panel for left side of game window (blue player controls)
+	JPanel bluePlayerOptions = new JPanel();
+	//creates label for blue player buttons
+	JLabel bluePlayerTxt = new JLabel("Blue Player");
+	//button group for S and O buttons
+	ButtonGroup SOS_Buttons_B = new ButtonGroup();
+	//s button for blue player
+	JRadioButton Blue_S = new JRadioButton();
+	//o button for blue player
+	JRadioButton Blue_O = new JRadioButton();
+	
+	//button group for human and computer buttons (blue player)
+	ButtonGroup B_PlayerType = new ButtonGroup();
+	//computer player button (blue player)
+	JRadioButton B_Computer = new JRadioButton();
+	//button for human (blue player)
+	JRadioButton B_Human = new JRadioButton();
+	
+
+	
+	//
+	//Panel for right of game window (red player controls)
+	JPanel redPlayerOptions = new JPanel();
+	//creates label for blue player buttons
+	JLabel redPlayerTxt = new JLabel("Red Player");
+	//button group for S and O buttons
+	ButtonGroup SOS_Buttons_R = new ButtonGroup();
+			
+	JRadioButton Red_S = new JRadioButton();
+	JRadioButton Red_O = new JRadioButton();
+	
+	//button group for human and computer buttons
+	ButtonGroup R_PlayerType = new ButtonGroup();
+	
+	//button for human player
+	JRadioButton R_Human = new JRadioButton();
+	
+	//button for computer player
+	JRadioButton R_Computer = new JRadioButton();
+	
+	//
+	//Panel for bottom of game window
+	JPanel gameStatusBar = new JPanel(new FlowLayout());
+	JLabel gameStatusText = new JLabel();
+
+	
+	
+	JButton newGameButton = new JButton();
+	
+	
+	
+	
+	
+	
+	
+	
+	public SOS_GUI(Board board) {
+		this.board = board;
+		//set simple and general game button texts
+		//simple.setText("Simple Game");
+		//general.setText("General Game");
+		//disable blue player buttons because red player goes first
+		//set default symbol for blue player
+		//Blue_S.setSelected(true);
+		//set default symbol for red player
+		//Red_S.setSelected(true);
+		//board.setPlayerSymbol(Red_S.getText().charAt(0));
+		//default game type
+		//simple.setSelected(true);
+		//board.setGameMode(simple.getText());
+		setContentPane();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		pack(); 
+		setTitle("SOS Game");
+		setVisible(true);  
+	}
+
+	public Board getBoard() {
+		return board;
+	}
+	
+	
+	public void updateTurns(String playerColor, char playerSym) {
+		//display current turn
+		gameStatusText.setText("Current Turn: " + playerColor);
+		//debug code
+		//playerSymbol.setText("Current Symbol: " + playerSym);
+		//disable radio buttons for other player
+		if(playerColor == "red") {
+			Blue_S.setEnabled(false);
+			Blue_O.setEnabled(false);
+			Red_S.setEnabled(true);
+			Red_S.setSelected(true);
+			Red_O.setEnabled(true);
+			board.setPlayerSymbol(Red_S.getText().charAt(0));
+		}
+		else {
+			Blue_S.setEnabled(true);
+			Blue_S.setSelected(true);
+			Blue_O.setEnabled(true);
+			Red_S.setEnabled(false);
+			Red_O.setEnabled(false);
+			board.setPlayerSymbol(Blue_S.getText().charAt(0));
+		}
+	}
+
+
+	
+	private void setContentPane(){
+		
+		//
+		newGameButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//set the board size and repaint grid canvas
+				
+				if(Integer.parseInt(tf.getText()) < 0) {
+					board.setBoardsize(0);
+				}
+				else {
+					board.setBoardsize(Integer.parseInt(tf.getText()));
+				}
+				
+				//set the game mode
+				if(simple.isSelected()) {
+					board.setGameMode(simple.getText());
+				}
+				else if(general.isSelected()) {
+					board.setGameMode(general.getText());
+				}
+				if(board.getPlayerColor() == "red") {
+					Blue_S.setEnabled(false);
+					Blue_O.setEnabled(false);
+				}
+				else if(board.getPlayerColor() == "blue") {
+					Red_S.setEnabled(false);
+					Red_O.setEnabled(false);
+				}
+				canvas.paintComponent(canvas.getGraphics());
+				//resize JFrame (window) according to contents
+				pack();
+				//removes focus from text box
+				tf.getRootPane().requestFocus();
+			}
+			
+		});
+		
+		/*
+		simple.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				board.setGameMode(simple.getText());
+				
+			}
+			
+		});
+		
+		general.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				board.setGameMode(general.getText());	
+			}
+			
+		});
+		*/
+		
+		simple.setText("Simple Game");
+		general.setText("General Game");
+
+
+
+		newGameButton.setText("New Game");
+		gameType.add(simple);
+		gameType.add(general);
+		gameOptions.add(title);
+		gameOptions.add(simple);
+		gameOptions.add(general);
+		gameOptions.add(sizeFieldTxt);
+		gameOptions.add(tf);
+		gameOptions.add(newGameButton);
+		//
+		//
+		
+		
+		bluePlayerOptions.setLayout(new BoxLayout(bluePlayerOptions, BoxLayout.Y_AXIS));
+		bluePlayerOptions.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+
+		Blue_S.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				board.setPlayerSymbol(Blue_S.getText().charAt(0));
+			}	
+		});
+		
+		
+		Blue_O.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				board.setPlayerSymbol(Blue_O.getText().charAt(0));
+			}
+			
+		});
+		
+		Blue_S.setText("S");
+		Blue_O.setText("O");
+		
+		B_Human.setText("Human");
+		B_Computer.setText("Computer");
+		
+		
+		SOS_Buttons_B.add(Blue_S);
+		SOS_Buttons_B.add(Blue_O);
+		
+		B_PlayerType.add(B_Human);
+		B_PlayerType.add(B_Computer);
+		
+		bluePlayerOptions.add(bluePlayerTxt);
+		bluePlayerOptions.add(B_Human);
+		bluePlayerOptions.add(Blue_S);
+		bluePlayerOptions.add(Blue_O);
+		bluePlayerOptions.add(B_Computer);
+		
+		
+		redPlayerOptions.setLayout(new BoxLayout(redPlayerOptions, BoxLayout.Y_AXIS));
+		redPlayerOptions.setBorder(new EmptyBorder(10, 10, 10, 10));
+		
+		
+		Red_S.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				board.setPlayerSymbol(Red_S.getText().charAt(0));
+				
+			}
+			
+		});
+
+		Red_O.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				board.setPlayerSymbol(Red_O.getText().charAt(0));
+				
+			}
+			
+		});
+		Red_S.setText("S");
+		Red_O.setText("O");
+		R_Human.setText("Human");
+		R_Computer.setText("Computer");
+		
+		
+		SOS_Buttons_R.add(Red_S);
+		SOS_Buttons_R.add(Red_O);
+		
+		R_PlayerType.add(R_Human);
+		R_PlayerType.add(R_Computer);
+		
+		redPlayerOptions.add(redPlayerTxt);
+		redPlayerOptions.add(R_Human);
+		redPlayerOptions.add(Red_S);
+		redPlayerOptions.add(Red_O);
+		redPlayerOptions.add(R_Computer);
+		//
+		//
+		
+		gameStatusText.setText("Current Turn: " + board.getPlayerColor());
+		
+		//debug code
+		//playerSymbol.setText("Current Symbol: " + board.getPlayerSymbol());
+
+		gameStatusBar.add(gameStatusText);
+		//debug
+		//gameStatusBar.add(playerSymbol);
+
+		//
+		//
+		//Panel for center of game window (game board)
+		canvas = new Canvas();
+		
+
+		//newGame.add(newGameButton);
+		
+	
+
+		//
+		//Container for entire game window
+		Container contentPane = getContentPane();
+		contentPane.setLayout(new BorderLayout());
+		//add board to center of window and game options to top of window
+		contentPane.add(canvas, BorderLayout.CENTER);
+		contentPane.add(gameOptions, BorderLayout.NORTH);
+		contentPane.add(bluePlayerOptions, BorderLayout.WEST);
+		contentPane.add(redPlayerOptions, BorderLayout.EAST);
+		contentPane.add(gameStatusBar, BorderLayout.SOUTH);
+		//
+		//
+	}
+
+	class Canvas extends JPanel {
+		
+		Canvas(){
+			addMouseListener(new MouseAdapter() {
+				public void mouseClicked(MouseEvent e) {
+					if(board.getGameState() == GameState.PLAYING) {
+						int rowSelected = e.getY() / CELL_SIZE;
+						int colSelected = e.getX() / CELL_SIZE;
+						board.makeMove(rowSelected, colSelected);
+						updateTurns(board.getPlayerColor(), board.getPlayerSymbol());
+						
+					}
+					else {
+						board.InitializeGrid();
+					}
+					repaint();
+
+				}
+				
+			});
+		}
+
+		
+		@Override
+		public void paintComponent(Graphics g) {
+			CANVAS_WIDTH = CELL_SIZE * board.getBoardsize();  
+			CANVAS_HEIGHT = CELL_SIZE * board.getBoardsize();
+			setPreferredSize(new Dimension(CANVAS_WIDTH, CANVAS_HEIGHT));
+			super.paintComponent(g);   
+			setBackground(Color.WHITE);
+			drawGridLines(g);
+			drawBoard(g);
+			printStatusBar();
+		}
+		
+		private void drawGridLines(Graphics g){
+			g.setColor(Color.LIGHT_GRAY);
+			for (int row = 1; row < board.getBoardsize(); row++) {
+				g.fillRoundRect(0, CELL_SIZE * row - GRID_WIDHT_HALF,
+						CANVAS_WIDTH-1, GRID_WIDTH, GRID_WIDTH, GRID_WIDTH);
+			}
+			for (int col = 1; col < board.getBoardsize(); col++) {
+				g.fillRoundRect(CELL_SIZE * col - GRID_WIDHT_HALF, 0,
+						GRID_WIDTH, CANVAS_HEIGHT-1, GRID_WIDTH, GRID_WIDTH);
+			}
+
+		}
+
+		private void drawBoard(Graphics g){
+			Graphics2D g2d = (Graphics2D)g;
+			g2d.setStroke(new BasicStroke(SYMBOL_STROKE_WIDTH, BasicStroke.CAP_ROUND, BasicStroke.JOIN_ROUND)); 
+			for (int row = 0; row < board.getBoardsize(); row++) {
+				for (int col = 0; col < board.getBoardsize(); col++) {
+					int x1 = col * CELL_SIZE + CELL_PADDING;
+					int y1 = row * CELL_SIZE + CELL_PADDING;
+					if(board.getCellClr(row, col) == "red") {
+						g2d.setColor(Color.RED);
+					}
+					else {
+						g2d.setColor(Color.BLUE);
+					}
+					if(board.getCellSym(row, col) == 1) {
+						int y2 = (row + 1) * CELL_SIZE - CELL_PADDING;
+						g2d.setFont(new Font("TimesRoman", Font.BOLD, CELL_SIZE));
+						//g2d.setColor(Color.BLACK);
+						g2d.drawString("S", x1, y2);	
+					}
+					else if(board.getCellSym(row,col) == 2) {
+						//g2d.setColor(Color.BLACK);
+						g2d.drawOval(x1, y1, SYMBOL_SIZE, SYMBOL_SIZE);
+					}			
+				}
+			}
+		}
+		private void printStatusBar() {
+			if(board.getGameState() == GameState.PLAYING) {
+				gameStatusText.setForeground(Color.BLACK);
+				updateTurns(board.getPlayerColor(), board.getPlayerSymbol());
+			}
+			else if(board.getGameState() == GameState.DRAW) {
+				gameStatusText.setForeground(Color.MAGENTA);
+				gameStatusText.setText("It's a Draw! Click to play again.");
+			}
+			else if(board.getGameState() == GameState.RED_WON) {
+				gameStatusText.setForeground(Color.RED);
+				gameStatusText.setText("Red Player Won! Click to play again.");
+			}
+			else if(board.getGameState() == GameState.BLUE_WON) {
+				gameStatusText.setForeground(Color.BLUE);
+				gameStatusText.setText("Blue Player Won! Click to play again.");
+			}
+		}
+
+	}
+
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				new SOS_GUI(new Board()); 
+			}
+		});
+	}
+
+
+}
